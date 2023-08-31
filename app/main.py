d82366974751ef14475a8cf201577bcbb3585db5
ProkopMax <72852008+ProkopMax@users.noreply.github.com>
2023-08-31 16:17:34 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import google_data, insert_main_data, view_all_content, view_all_fields
+from flask import Flask, render_template, request
+from database import google_data, insert_main_data, view_all_content, view_all_fields, select_content
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +9,15 @@ def index():
     update_content = insert_main_data(google_data(), 'byte1')
     return render_template('index.html', content=content, labels=fields, update_content=update_content)
 
+@app.route('/search', methods=["GET", "POST"])
+def search():
+    fields = view_all_fields('byte1')
+    if request.method == "POST":
+      data = dict(request.form)
+      content = select_content('byte1', data["search"])
+    else:
+      content = []
+    return render_template('search.html', content=content, labels=fields)
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000", debug=True)
-
