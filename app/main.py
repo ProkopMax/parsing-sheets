6ae51c9197e1da783e4c1e733ea29c6bf57c341e
Prokopenko Maksim <prokopenkomax@gmail.com>
2023-09-15 11:18:27 +0200

    add gunicorn for stable work and change user to none root

--- app/main.py
+++ app/main.py
@@ -30,4 +30,4 @@ def update_all():
     return render_template('update_all.html', count=count)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(debug=True)
--- app/utils.py
+++ app/utils.py
@@ -1,21 +1,10 @@
-import requests
-import csv
-import io
-import urllib3
-from settings import HEADERS_USER_AGENT, HEADERS_DNT, HEADERS_ACCEPT, HEADERS_ACCEPT_CHARSET, HEADERS_ACCEPT_ENCODING, HEADERS_ACCEPT_LANGUAGE, GOOGLE_FILE_ID, GOOGLE_URL_CSV
+import time
 
-# Disable ssl warninngs
-urllib3.disable_warnings()
+def get_time():
+    try:
+        curr_time = time.strftime("%H:%M", time.localtime())
+        return curr_time
+    except:
+        print("Ошибка получения ВРЕМЕНИ")
 
-# Pull data from google file
-def get_data():
-    #headers={"User-Agent": HEADERS_USER_AGENT, "DNT": HEADERS_DNT, "Accept": HEADERS_ACCEPT, "Accept-Charset": HEADERS_ACCEPT_CHARSET, "Accept-Encoding": HEADERS_ACCEPT_ENCODING, "Accept-Language": HEADERS_ACCEPT_LANGUAGE}
-    lines = []
-    url = GOOGLE_URL_CSV.format(GOOGLE_FILE_ID)
-    r = requests.get(url, verify=False)
-    data = {}
-    cols = []
-    sio = io.StringIO( r.content.decode('utf-8'), newline=None)
-    reader = csv.reader(sio, dialect=csv.excel)
-    rownum = 0
-    return reader
+
