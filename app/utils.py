2aa5e26872eb7773928852d0c23183f5ee244b44
ProkopMax <72852008+ProkopMax@users.noreply.github.com>
2025-03-27 14:52:26 +0300

    add loger

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,5 @@
 import time
+import logging
 
 def get_time():
     try:
@@ -6,5 +7,15 @@ def get_time():
         return curr_time
     except:
         print("Ошибка получения ВРЕМЕНИ")
+
+def get_module_logger(mod_name):
+    logger = logging.getLogger(mod_name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
 
 
